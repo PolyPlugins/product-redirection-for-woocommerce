d7c63f5df8e1b7d831f8bbba3f250e1d2bfd34f5
Poly Plugins <contact@polyplugins.com>
2025-08-14 05:06:10 -0400

    Updated: Handling for bulk edits to keep existing values

--- includes/classes/Backend/Edit.php
+++ includes/classes/Backend/Edit.php
@@ -44,8 +44,8 @@ class Edit {
   public function init() {
     add_filter('manage_edit-product_columns', array($this, 'add_columns'));
     add_action('manage_product_posts_custom_column', array($this, 'render_columns'), 10, 2);
-    add_action('quick_edit_custom_box', array($this, 'add_edit_fields'), 10, 2);
-    add_action('bulk_edit_custom_box', array($this, 'add_edit_fields'), 10, 2);
+    add_action('quick_edit_custom_box', array($this, 'add_quick_edit_fields'), 10, 2);
+    add_action('bulk_edit_custom_box', array($this, 'add_bulk_edit_fields'), 10, 2);
     add_action('save_post_product', array($this, 'save_quick_edit'), 10, 2);
     add_action('save_post_product', array($this, 'save_bulk_edit'), 10, 2);
   }
@@ -108,7 +108,7 @@ class Edit {
    * @param  mixed $post_type
    * @return void
    */
-  public function add_edit_fields($column_name, $post_type) {
+  public function add_quick_edit_fields($column_name, $post_type) {
     switch( $column_name ) {
       case 'enable_prfw': {
         ?>
@@ -152,6 +152,87 @@ class Edit {
             <span class="title"><?php esc_html_e('Redirect To', 'product-redirection-for-woocommerce'); ?></span>
             <span class="input-text-wrap">
               <select name="redirect_to_prfw">
+                <option value="This Product's Category"><?php esc_html_e("This Product's Category", 'product-redirection-for-woocommerce'); ?></option>
+                <option value="Custom"><?php esc_html_e('Custom', 'product-redirection-for-woocommerce'); ?></option>
+            </select>
+            </span>
+          </label>
+        </div>
+        <?php
+        break;
+      }
+      
+      case 'redirect_url_prfw': {
+        ?>
+            <div class="inline-edit-col">
+              <label>
+                <span class="title"><?php esc_html_e('Redirect URL', 'product-redirection-for-woocommerce'); ?></span>
+                <span class="input-text-wrap">
+                  <input type="url" name="redirect_url_prfw" value="">
+                </span>
+              </label>
+            </div>
+          </fieldset>
+        <?php
+        break;
+      }
+    }
+  }
+  
+  /**
+   * Bulk edit fields
+   *
+   * @param  mixed $column_name
+   * @param  mixed $post_type
+   * @return void
+   */
+  public function add_bulk_edit_fields($column_name, $post_type) {
+    switch( $column_name ) {
+      case 'enable_prfw': {
+        ?>
+        <fieldset class="inline-edit-col-left">
+          <div class="inline-edit-col">
+            <label>
+              <span class="title"><?php esc_html_e('PRFW Type', 'product-redirection-for-woocommerce'); ?></span>
+              <span class="input-text-wrap">
+                <select name="enable_prfw">
+                  <option value="">— No Change —</option>
+                  <option value="Disabled"><?php esc_html_e('Disabled', 'product-redirection-for-woocommerce'); ?></option>
+                  <option value="Redirect"><?php esc_html_e('Redirect', 'product-redirection-for-woocommerce'); ?></option>
+                </select>
+              </span>
+            </label>
+          </div>
+        <?php
+        break;
+      }
+
+      case 'redirect_type_prfw': {
+        ?>
+        <div class="inline-edit-col">
+          <label>
+            <span class="title"><?php esc_html_e('Redirect Type', 'product-redirection-for-woocommerce'); ?></span>
+            <span class="input-text-wrap">
+              <select name="redirect_type_prfw">
+                <option value="">— No Change —</option>
+                <option value="301"><?php esc_html_e('Permanent', 'product-redirection-for-woocommerce'); ?></option>
+                <option value="302"><?php esc_html_e('Temporary', 'product-redirection-for-woocommerce'); ?></option>
+              </select>
+            </span>
+          </label>
+        </div>
+        <?php
+        break;
+      }
+      
+      case 'redirect_to_prfw': {
+        ?>
+        <div class="inline-edit-col">
+          <label>
+            <span class="title"><?php esc_html_e('Redirect To', 'product-redirection-for-woocommerce'); ?></span>
+            <span class="input-text-wrap">
+              <select name="redirect_to_prfw">
+                <option value="">— No Change —</option>
                 <option value="This Product's Category"><?php esc_html_e("This Product's Category", 'product-redirection-for-woocommerce'); ?></option>
                 <option value="Custom"><?php esc_html_e('Custom', 'product-redirection-for-woocommerce'); ?></option>
             </select>
@@ -240,19 +321,19 @@ class Edit {
       return;
     }
 
-    if (isset($_REQUEST['enable_prfw'])) {
+    if (isset($_REQUEST['enable_prfw']) && $_REQUEST['enable_prfw']) {
       update_field('enable_prfw', sanitize_text_field(wp_unslash($_REQUEST['enable_prfw'])), $post_id);
     }
 
-    if (isset($_REQUEST['redirect_type_prfw'])) {
+    if (isset($_REQUEST['redirect_type_prfw']) && $_REQUEST['redirect_type_prfw']) {
       update_field('redirect_type_prfw', sanitize_text_field(wp_unslash($_REQUEST['redirect_type_prfw'])), $post_id);
     }
 
-    if (isset($_REQUEST['redirect_to_prfw'])) {
+    if (isset($_REQUEST['redirect_to_prfw']) && $_REQUEST['redirect_to_prfw']) {
       update_field('redirect_to_prfw', sanitize_text_field(wp_unslash($_REQUEST['redirect_to_prfw'])), $post_id);
     }
     
-    if (isset($_REQUEST['redirect_url_prfw'])) {
+    if (isset($_REQUEST['redirect_url_prfw']) && $_REQUEST['redirect_url_prfw']) {
       update_field('redirect_url_prfw', sanitize_url(wp_unslash($_REQUEST['redirect_url_prfw'])), $post_id);
     }
   }
